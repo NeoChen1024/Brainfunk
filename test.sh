590a5531e8b04b52924ff66ec2aee1a0a99f73ae
Neo_Chen <chenkolei@gmail.com>
2018-09-30 17:29:48 +0800

    Test: Download things from Internet to do more test

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
+set -e
+
+test_echo()
+{
+	printf "\033[44m==>>\033[0m \033[1;33m%s\033[0m\t\t\033[1;32m%s\033[0m\n" "$1" "$2"
+}
 
 msg_echo()
 {
-	printf "\033[44m==>>\033[0m \033[1;33m%s\033[0m\t\t\033[1;32m%s\033[0m\n" "$1" "$2"
+	printf "\033[44m==>>\033[0m \033[1;33m%s\n" "$1"
 }
 
 response()
@@ -14,3 +20,17 @@ msg_echo "Testing Basic Loop, see if it coredumps" "-[>+.<-]"
 
 msg_echo "Hello World Test" '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
 ( ./brainfunk -c '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.' ) && response "PASS"
+
+msg_echo "Downloading More Tests from Internet"
+mkdir -p test
+
+cd test
+curl -OJv http://www.kotay.com/sree/bf/brainf_progs.tar
+tar -xpf brainf_progs.tar
+
+for i in *.b ; do
+	msg_echo "Testing ${i%.b}"
+	../brainfunk -f $i && response "PASS"
+done
+
+cd ..
