92cd92d7f4d3a2ecbee2cad41b420e6902e2a5f0
Neo_Chen <chenkolei@gmail.com>
2018-10-03 22:34:53 +0800

    Test: Refactored Code for testing codes from remote url

--- test.sh
+++ test.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-test_echo()
-{
-	printf "\033[44m==>>\033[0m \033[1;33m%s\033[0m\t\t\033[1;32m%s\033[0m\n" "$1" "$2"
-}
+if [ -z "${BF}" ]; then
+	BF="`realpath brainfunk`"
+fi
 
 msg_echo()
 {
@@ -15,27 +14,45 @@ response()
 {
 	printf "\033[30;43m<<==\033[0m \033[1;32m%s\033[0m\n" "$1"
 }
-msg_echo "Testing Basic Loop, see if it coredumps" "-[>+.<-]"
-( ./brainfunk -c '-[>+<-]' ) && response "PASS"
 
-msg_echo "Hello World Test" '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
-( ./brainfunk -c '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.' ) && response "PASS"
+fetch()
+{
+	local url="$1"
+	if [ -f "${i##*/}" ]; then
+		curl -OJv "$url"
+	fi
+}
+
+test_from_url()
+{
+	local url="$1"
+	local file="${1##*/}"
+	fetch "$url"
+	msg_echo "Testing $file"
+
+	${BF} -f "$file" && response "PASS" || response "FAIL"
+}
+
+
+msg_echo "Testing Basic Loop, see if it coredumps" "-[>+<-]"
+( ${BF} -c '-[>+<-]' ) && response "PASS"
+
+msg_echo "Hello World Test"
+( ${BF} -c '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.' ) && response "PASS"
 
 msg_echo "Downloading More Tests from Internet"
 mkdir -p test
 
 cd test
-[ -f brainf_progs.tar ] || curl -OJv http://www.kotay.com/sree/bf/brainf_progs.tar
+fetch http://www.kotay.com/sree/bf/brainf_progs.tar
 tar -xpf brainf_progs.tar
 
-for i in bench.b long.b fib.b ; do
+for i in bench.b long.b; do
 	msg_echo "Testing ${i%.b}"
-	../brainfunk -f $i && response "PASS"
+	${BF} -f $i && response "PASS"
 done
 
-[ -f mandelbrot.b ] || curl -OJv http://esoteric.sange.fi/brainfuck/utils/mandelbrot/mandelbrot.b
-
-msg_echo "Testing mandelbrot"
-../brainfunk -f mandelbrot.b && response "PASS"
+test_from_url https://raw.githubusercontent.com/pablojorge/brainfuck/master/programs/sierpinski.bf
+test_from_url http://esoteric.sange.fi/brainfuck/utils/mandelbrot/mandelbrot.b
 
 cd ..
