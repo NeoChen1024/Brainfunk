0e3b308fb420325a83c4610eb6637bb97d8c8f2e
Neo_Chen <chenkolei@gmail.com>
2018-10-08 17:22:51 +0800

    Test: Fixed fetch()

--- test.sh
+++ test.sh
@@ -2,9 +2,7 @@
 set -e
 
 if [ -z "${BF}" ]; then
-	BF="`realpath brainfunk`"
-else
-	BF="`realpath $BF`"
+	BF="${PWD}/brainfunk"
 fi
 
 msg_echo()
@@ -20,7 +18,7 @@ response()
 fetch()
 {
 	local url="$1"
-	if [ -f "${i##*/}" ]; then
+	if [ ! -f "${i##*/}" ]; then
 		msg_echo "Fetching $url"
 		curl -OJv "$url"
 	fi
